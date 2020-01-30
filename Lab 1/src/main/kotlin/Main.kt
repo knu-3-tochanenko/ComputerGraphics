47efd844eabd8b033054a0ee05f7f69dc8a3256a
Vladislav <itochanenko@gmail.com>
2020-01-30 07:56:01 +0200

    Fixed bugs
    Fixed bug in algorithm. Cleaned code

--- Lab 1/src/main/kotlin/Main.kt
+++ Lab 1/src/main/kotlin/Main.kt
@@ -1,16 +1,20 @@
+import kotlin.math.atan2
+
 class Main {
     companion object {
         var vertexes = mutableListOf<Dot>()
         var dot: Dot
 
         init {
-            vertexes.add(Dot(0.0, 0.0))
             vertexes.add(Dot(0.0, 1.0))
+            vertexes.add(Dot(0.0, 0.0))
             vertexes.add(Dot(2.0, 2.0))
             vertexes.add(Dot(4.0, 2.0))
             vertexes.add(Dot(3.0, 0.0))
             vertexes.add(Dot(0.5, -1.0))
-            dot = Dot(1.1, 1.1)
+            vertexes.sortBy { -atan2(-it.x, -it.y) }
+//            dot = Dot(-0.1, 0.9)
+            dot = Dot(1.0, 1.0)
         }
 
         fun rotation(a: Dot, b: Dot, c: Dot): Double = ((b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x))
@@ -20,9 +24,7 @@ class Main {
         }
 
         fun checkIfInside(): Boolean {
-            val len = vertexes.size;
-            println(rotation(vertexes[0], vertexes[1], dot))
-            println(rotation(vertexes[0], vertexes[len - 1], dot))
+            val len = vertexes.size
             if ((rotation(vertexes[0], vertexes[1], dot) < 0)
                 || (rotation(vertexes[0], vertexes[len - 1], dot) > 0)
             )
@@ -32,8 +34,8 @@ class Main {
             var r = len - 1
             var m: Int
 
-            while (l - r > 1) {
-                m = (l + r) / 2
+            while (r - l > 1) {
+                m = l+(r-l) / 2
                 if (rotation(vertexes[0], vertexes[m], dot) < 0) r = m
                 else l = m
             }
