2aa901a77cee3037b32893a4f0da432c968f1703
Vladislav <itochanenko@gmail.com>
2020-02-12 13:46:44 +0200

    Formatted code

--- Lab 1/src/main/kotlin/Main.kt
+++ Lab 1/src/main/kotlin/Main.kt
@@ -19,9 +19,9 @@ class Main {
 
         fun rotation(a: Dot, b: Dot, c: Dot): Double = ((b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x))
 
-        fun intersect(a: Dot, b: Dot, c: Dot, d: Dot): Boolean {
-            return (rotation(a, b, c) * rotation(a, b, d) <= 0) && (rotation(c, d, a) * rotation(c, d, b) < 0)
-        }
+        fun intersect(a: Dot, b: Dot, c: Dot, d: Dot): Boolean =
+            (rotation(a, b, c) * rotation(a, b, d) <= 0)
+                    && (rotation(c, d, a) * rotation(c, d, b) < 0)
 
         fun checkIfInside(): Boolean {
             val len = vertexes.size
@@ -35,7 +35,7 @@ class Main {
             var m: Int
 
             while (r - l > 1) {
-                m = l+(r-l) / 2
+                m = l + (r - l) / 2
                 if (rotation(vertexes[0], vertexes[m], dot) < 0) r = m
                 else l = m
             }
