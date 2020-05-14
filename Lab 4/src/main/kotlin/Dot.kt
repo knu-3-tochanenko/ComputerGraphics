8ed24ee54adb06c71336ef23e83cd7bf6d11a75a
Vladislav <itochanenko@gmail.com>
2020-05-14 09:31:25 +0300

    Init project for Lab 4

---
+++ Lab 4/src/main/kotlin/Dot.kt
@@ -0,0 +1,5 @@
+data class Dot(
+    var x: Double,
+    var y: Double,
+    var angle: Double = 0.0
+)
---
+++ Lab 4/src/main/kotlin/Tree2D.kt
@@ -0,0 +1,10 @@
+class Tree2D {
+    companion object {
+        private var result = mutableListOf<Dot>()
+
+        fun calculate(dots: List<Dot>): List<Dot> {
+
+            return result
+        }
+    }
+}
