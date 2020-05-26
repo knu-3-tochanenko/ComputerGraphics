8872cfbbbc0cd7258513037b2f0d63a8ff4ba803
Vladislav <itochanenko@gmail.com>
2020-05-26 14:50:12 +0300

    Add Chart classes

---
+++ Lab 2/src/main/kotlin/Chart.kt
@@ -0,0 +1,59 @@
+import org.knowm.xchart.QuickChart
+import org.knowm.xchart.SwingWrapper
+import org.knowm.xchart.XYChart
+import org.knowm.xchart.style.lines.SeriesLines
+import org.knowm.xchart.style.markers.SeriesMarkers
+import java.awt.Color
+
+class Chart(
+    private val chartName: String = "Chart",
+    private val xName: String = "x",
+    private val yName: String = "y"
+) {
+
+    private val chart: XYChart =
+        QuickChart.getChart(
+            chartName, xName, yName, "0, 0",
+            doubleArrayOf(0.0), doubleArrayOf(0.0)
+        )
+
+    private var lines = mutableListOf<ChartLine>()
+    private var dots = mutableListOf<Dot>()
+    private var highlightedDots = mutableListOf<Dot>()
+
+    fun addLine(dots: List<Dot>) {
+        lines.add(ChartLine(dots))
+    }
+
+    fun addDots(dots: List<Dot>) {
+        for (i in dots.indices) {
+            this.dots.add(dots[i])
+        }
+    }
+
+    fun addHighlightedDots(dots: List<Dot>) {
+        for (i in dots.indices) {
+            this.highlightedDots.add(dots[i])
+        }
+    }
+
+    fun addLine(line: ChartLine) {
+        lines.add(line)
+    }
+
+    fun draw() {
+        for (line in lines)
+            chart.addSeries(line.lineName, line.xData, line.yData)
+                .setMarker(SeriesMarkers.CIRCLE).setMarkerColor(Color.BLUE).setLineColor(Color.BLUE).setLineStyle(SeriesLines.DASH_DASH)
+
+        for (i in dots.indices)
+            chart.addSeries("dot#$i", doubleArrayOf(dots[i].x), doubleArrayOf(dots[i].y))
+                .setMarkerColor(Color.BLACK).setMarker(SeriesMarkers.CIRCLE)
+
+        for (i in highlightedDots.indices)
+            chart.addSeries("dot#${i + dots.size}", doubleArrayOf(highlightedDots[i].x), doubleArrayOf(highlightedDots[i].y))
+                .setMarkerColor(Color.RED).setMarker(SeriesMarkers.CIRCLE)
+
+        SwingWrapper(chart).displayChart()
+    }
+}
